bc4501eb547f576eac91aef3f3b42da96744f982
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-04-17 18:38:18 +0300

    дз

--- mobile/android/test_wikipedia_android.py
+++ mobile/android/test_wikipedia_android.py
@@ -6,16 +6,16 @@ from selene import browser, have, be
 
 
 def test_search_and_click(android_management):
-
-    with step('Searching "Appium"'):
+    with step('Поиск "Appium"'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")).should(be.visible)
 
-    with step('Verify found page'):
+    with step('Проверка найденных результатов'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
 
-    with step('Open "Appium" page'):
-        appium_page = results.first
-        appium_page.click()
+    with step('Открытие страницы "Appium"'):
+        results.first.click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/view_page_title_text")).should(have.text('Appium'))
